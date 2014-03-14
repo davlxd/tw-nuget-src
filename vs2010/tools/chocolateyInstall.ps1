0d8a050157845d425c817675babb0b44364be752
lxd <xdli@thoughtworks.com>
2014-03-14 18:42:43 +0800

    Make it continous

--- vs2010/tools/chocolateyInstall.ps1
+++ vs2010/tools/chocolateyInstall.ps1
@@ -6,10 +6,10 @@ $silentArgs = 'SILENT_ARGS_HERE'
 $validExitCodes = @(0)
 
 try {
-  Get-ChocolateyWebFile "$packageName" 'c:\vs2010.zip' "$url" "$url64"
-  Get-ChocolateyUnzip "c:\vs2010.zip" "c:\vs2010"
+  Get-ChocolateyWebFile "$packageName" 'c:\vs2010.zip' "$url" "$url64" -validExitCodes $validExitCodes
+  Get-ChocolateyUnzip "c:\vs2010.zip" "c:\vs2010" -validExitCodes $validExitCodes
   
-  Install-ChocolateyInstallPackage "$packageName" "$installerType" "/UnattendFile C:\vs2010\unattend-install-vs2010.ini" 'C:\vs2010\en_visual_studio_2010_ultimate_x86_dvd_509116\Setup\setup.exe'
+  Install-ChocolateyInstallPackage "$packageName" "$installerType" "/UnattendFile C:\vs2010\unattend-install-vs2010.ini" 'C:\vs2010\en_visual_studio_2010_ultimate_x86_dvd_509116\Setup\setup.exe' -validExitCodes $validExitCodes
   Write-ChocolateySuccess "$packageName"
 } catch {
   Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
